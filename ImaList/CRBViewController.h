b31f50fa6625799af19f5a6a14b2f7ff366d4e40
Chris Breiding <chrisbreiding@gmail.com>
2013-04-26 21:52:03 -0400

    Initial Commit

---
+++ ImaList/CRBViewController.h
@@ -0,0 +1,13 @@
+//
+//  CRBViewController.h
+//  ImaList
+//
+//  Created by CB on 04/26/13.
+//  Copyright (c) 2013 CB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CRBViewController : UIViewController
+
+@end
---
+++ ImaList/CRBViewController.m
@@ -0,0 +1,29 @@
+//
+//  CRBViewController.m
+//  ImaList
+//
+//  Created by CB on 04/26/13.
+//  Copyright (c) 2013 CB. All rights reserved.
+//
+
+#import "CRBViewController.h"
+
+@interface CRBViewController ()
+
+@end
+
+@implementation CRBViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
