2586433ec9af365449a8e1d47bf9ad0918345d13
Chris Breiding <chrisbreiding@gmail.com>
2016-11-15 22:09:39 -0500

    fancy passcode styling and behavior

--- web/src/auth/passcode-input.jsx
+++ web/src/auth/passcode-input.jsx
@@ -1,29 +1,52 @@
+import _ from 'lodash'
 import { observer } from 'mobx-react'
 import React, { Component } from 'react'
+
+const validKeys = /(?:\d|Backspace)/
+
+function replaceCharAt (str, index, replacement) {
+  return str.substr(0, index) + replacement + str.substr(index + replacement.length)
+}
 
 @observer
 class PasscodeInput extends Component {
   render () {
     return (
-      <input
-        ref='passcode'
-        className='passcode-input'
-        type='tel'
-        value={this.props.value}
-        onChange={this._updatePasscode}
-      />
+      <div className='passcode-input'>
+        {_.map(_.range(4), (index) => (
+          <input
+            key={index}
+            ref={`passcode${index}`}
+            type='tel'
+            value={this.props.value.charAt(index) || ''}
+            onClick={this.focus}
+            onKeyUp={this._updatePasscode(index)}
+          />
+        ))}
+      </div>
     )
   }
 
-  _updatePasscode = (e) => {
-    const value = e.target.value.toString()
-    if (value.length <= 4) {
-      this.props.onChange(value)
-    }
+  componentDidUpdate () {
+    this.focus()
   }
 
-  focus () {
-    this.refs.passcode.focus()
+  _updatePasscode = (index) => (e) => {
+    if (!validKeys.test(e.key)) return
+
+    const currentValue = this.props.value
+
+    const newValue = e.key === 'Backspace' ?
+      currentValue.substr(0, currentValue.length - 1) :
+      replaceCharAt(currentValue, index, e.key)
+
+    this.props.onChange(newValue)
+  }
+
+  focus = () => {
+    const valueLength = this.props.value.length
+    const focusIndex = valueLength > 3 ? 3 : valueLength
+    this.refs[`passcode${focusIndex}`].focus()
   }
 }
 
